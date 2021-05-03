7b906d33f1d8f52ab287b30fcc461a070a5db64b
InnerWisdom <68237197+InnerWisdom@users.noreply.github.com>
2021-05-03 19:46:30 +0400

    Update index.js

--- Desktop/ooocpy/deploy/index.js
+++ Desktop/ooocpy/deploy/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 const passport = require('passport')
 
 const playerRouter = require('./routes/playerRTS')
@@ -16,6 +16,9 @@ app.listen(port, () => {
     console.log(`Working on http://localhost:${port}`)
 });
 
+app.get('/', (req, res) => {
+    res.send(`Server is working!`)
+});
 app.use('/api', playerRouter);
 app.use('/api', teamRouter);
 app.use('/api', userRouter);
